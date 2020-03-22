61d78b890e4d8acc7a2e919afbc4f689792a1ef0
sergiyZinovyev <thrion1@gmail.com>
2020-03-22 14:52:37 +0200

    release v2 add https

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
-
+ 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
--- src/app/news/newsapi.service.ts
+++ src/app/news/newsapi.service.ts
@@ -6,7 +6,7 @@ import { Observable} from 'rxjs';
 @Injectable()
 export class NewsapiService {
 
-  url = 'http://newsapi.org/v2/top-headlines?' + 'country=us&' + 'apiKey=b0ea3e22738349f5a5e14ddf35c89298';
+  url = 'https://newsapi.org/v2/top-headlines?' + 'country=us&' + 'apiKey=b0ea3e22738349f5a5e14ddf35c89298';
 
   constructor(private http: HttpClient){}
 
